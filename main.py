4611736bce2ed5b3d73d7e38b3fa493959b6de30
Fly1ngL3xmi <fly1ngl3xmi@gmail.com>
2024-10-30 17:59:11 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from prettytable import PrettyTable
+from config.art import logo
+from config.cfg import MultiToolCommands, medias, UserInput, main, test
+
+print(logo[0])
+MultiToolCommands()
+medias()
+UserInput()
